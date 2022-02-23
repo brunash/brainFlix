ffc6389ef8690e20f614b538f4ec47d3586514a4
brunash <bobazso@yahoo.com>
2022-02-23 10:53:10 -0500

    finished header section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Header from './components/Header';
+import Header from './components/Header/header';
 import './App.scss';
 
 function App() {
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -3,18 +3,18 @@ import BrainFlixLogo from "../../assets/images/BrainFlix-logo.svg"
 import Avatar from "../../assets/images/Mohan-muruge.jpg"
 import UploadButton from "../../assets/images/upload.svg"
 import Magnifier from "../../assets/images/search.svg"
-import "./index.scss"
-
-const magnifierFunction = () => {
-    return <img src={Magnifier}/>
-}
+import "./header.scss"
+import "../../App.scss"
 
 const Header = () => {
     return (
         <header className="header">
             <img src={BrainFlixLogo} className="header__logo"/>
             <div className="header__search">
-                <input type="search" className="header__search--input" placeholder={`${magnifierFunction()} Search`}></input>
+                <div className="header__search--input-container">
+                    <img src={Magnifier} className="header__search--magnifier"/>
+                    <input type="search" className="header__search--input" placeholder='Search'></input>
+                </div>
                 <img src={Avatar} className="header__search--avatar"/>
             </div>    
             <button className="header__button"><img className="header__button--arrow" src={UploadButton}/>Upload<div className="header__button--blank-div"></div></button>
