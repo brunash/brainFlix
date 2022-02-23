e795dcb08bd41bc07e32aee48fb76423620d3798
brunash <bobazso@yahoo.com>
2022-02-23 14:30:09 -0500

    started video branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header/header';
+import Video from './components/Video/video';
 import './App.scss';
 
 function App() {
   return (
     <div>
       <Header />
+      <Video  />
     </div>
   );
 }
