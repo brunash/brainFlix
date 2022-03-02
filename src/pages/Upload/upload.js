37aad055fd8525dbfa7c90a5e83cfc7616d84f6f
brunash <bobazso@yahoo.com>
2022-03-02 11:26:33 -0500

    finished styling for upload page-mobile size

--- src/pages/Upload/upload.js
+++ src/pages/Upload/upload.js
@@ -1,23 +1,27 @@
 // import Header from '../../components/Header/header';
 import "../../App.scss"
+import "./upload.scss"
+import Thumbnail from "../../assets/images/Upload-video-preview.jpg"
+import PublishLogo from "../../assets/images/publish.svg"
 
 function UploadPage () {
     return (
-        <>
+        <>  
+            <hr className="top-border"></hr>
             <article className='upload'>
                 <h1 className='upload__title'>Upload Video</h1>
                 <hero className='upload__hero'>
                     <p className='upload__hero--title'>VIDEO THUMBNAIL</p>
-                    <img className='upload__hero--img' />
+                    <img className='upload__hero--img' src={Thumbnail}/>
                 </hero>
                 <form className='upload__form'>
-                    <label className='upload__form--label' >TITLE YOUR VIDEO</label>
-                    <input className='upload__form--input'></input>
-                    <label className='upload__form--label'>TITLE YOUR VIDEO</label>
-                    <textarea className='upload__form--textarea' />
+                    <label className='upload__form--label' for="title">TITLE YOUR VIDEO</label>
+                    <input className='upload__form--input' name="title" placeholder="Add a title to your video"></input>
+                    <label className='upload__form--label' for="description">ADD A VIDEO DESCRIPTION</label>
+                    <textarea className='upload__form--textarea' name="decription" placeholder="Add a description to your video"/>
                     <div className='upload__form--button'>
-                        <button className='upload__form--button-publish'>PUBLISH</button>
-                        <button className='upload__form--button--cancel'>CANCEL</button>
+                        <button  className='upload__form--button--publish'><img src={PublishLogo}/>PUBLISH<div></div></button>
+                        <button className='upload__form--button--cancel'><img src={PublishLogo}/>CANCEL<div></div></button>
                     </div>
 
                 </form>
