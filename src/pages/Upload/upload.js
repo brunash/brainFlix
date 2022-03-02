99d1b4b065a5ad0f7b9700366c551b05fb0f4536
brunash <bobazso@yahoo.com>
2022-03-02 10:39:02 -0500

    added static html and classNames to upload page

--- src/pages/Upload/upload.js
+++ src/pages/Upload/upload.js
@@ -1,10 +1,28 @@
-import Header from '../../components/Header/header';
+// import Header from '../../components/Header/header';
 import "../../App.scss"
 
 function UploadPage () {
     return (
         <>
-        <div></div>
+            <article className='upload'>
+                <h1 className='upload__title'>Upload Video</h1>
+                <hero className='upload__hero'>
+                    <p className='upload__hero--title'>VIDEO THUMBNAIL</p>
+                    <img className='upload__hero--img' />
+                </hero>
+                <form className='upload__form'>
+                    <label className='upload__form--label' >TITLE YOUR VIDEO</label>
+                    <input className='upload__form--input'></input>
+                    <label className='upload__form--label'>TITLE YOUR VIDEO</label>
+                    <textarea className='upload__form--textarea' />
+                    <div className='upload__form--button'>
+                        <button className='upload__form--button-publish'>PUBLISH</button>
+                        <button className='upload__form--button--cancel'>CANCEL</button>
+                    </div>
+
+                </form>
+
+            </article>
         </>
     )
 }
